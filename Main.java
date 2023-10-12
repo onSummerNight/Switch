72ca60a8c305945d8ee675e48285bcf5e9e8a618
BuildTools <unconfigured@null.spigotmc.org>
2023-10-12 23:08:06 +0700

    add switch statement

--- Main.java
+++ Main.java
@@ -17,6 +17,23 @@ public class Main {
                 System.out.println("Was not 1, 2, 3, 4, or a 5");
                 break;
         }
-        // More code here
+
+        String month = "XYZ";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
     }
+
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
+    }
+
+
 }
